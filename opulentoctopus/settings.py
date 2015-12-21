0b141d87fb5e499fc19bb59bee7a746fc62b2755
Sarah Kelley <sarah.kelley@carneylabs.com>
2015-12-21 15:40:43 -0500

    switch to postgres db

--- opulentoctopus/settings.py
+++ opulentoctopus/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'opulentoctopus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'opulentoctopus',
+        'USER': 'carneyadmin',
+        'PASSWORD': 'password1',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
